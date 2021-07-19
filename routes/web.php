12f7868acb191e8e435a83dcfbfa194ee7ffaf6a
Anubhab <mukerji29anuvb@gmail.com>
2021-07-20 01:07:59 +0530

    controllers created; resource() route; category form working

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+// category routes starts
+
+// Route::get('/category/create', 'CategoryController@create');
+Route::resource('category', 'CategoryController');
+
+// category routes ends
