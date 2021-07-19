9eaf468f328f2f5d67b93b4c3ef0a9d8e60bdfa8
Anubhab <mukerji29anuvb@gmail.com>
2021-07-20 02:39:00 +0530

    CRUD:category index form works; edit button works

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,10 +8,41 @@
                 <div class="card-header">All Categories</div>
 
                 <div class="card-body">
-                    {{-- TODO: table to show data properly --}}
-                    @foreach ($categories as $category)
-                        <p>{{ $category->name }}</p>
-                    @endforeach
+                    {{-- TODO: table to show data properly - DONE: --}}
+                    <table class="table">
+                        <thead class="thead-dark">
+                          <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Category Name</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                        @if ( count( $categories ) > 0 )   
+                            @foreach ($categories as $key=>$category)
+                            <tr>
+                                <th scope="row">{{ $key+1 }}</th>
+                                <td>{{ $category->name }}</td>
+                                <td>
+                                    <a 
+                                        href="{{ route('category.edit',[$category->id]) }}"
+                                    >
+                                        <button class="btn btn-outline-info">Edit</button>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="">
+                                        <button class="btn btn-outline-danger">Delete</button>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        @else
+                            <td>No category to display</td>
+                        @endif
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
